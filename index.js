51a33e29d39d9ec83a454f1b6f1b1d49376ce7a6
mahabubsaki <md929820@gmail.com>
2022-11-07 09:01:24 +0600

    test

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT || 5000;
+
+const uri = "mongodb+srv://mahabub3:JhYWH49UTIy2W6Oa@cluster0.wcxgg.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const quizes = require('./quizes.json');
+
+const main = async () => {
+    const quizeCollection = client.db('triviamania').collection('quiztopics');
+    try {
+        app.get('/quizes', async (req, res) => {
+            try {
+                res.status(200).send({ message: 'Everything is OK', data: await quizeCollection.find({}).toArray() });
+            }
+            catch (err) {
+                res.status(500).send({ message: 'Internal server error', data: [] });
+            }
+        });
+        app.get('/quizes/:qid', async (req, res) => {
+            try {
+                res.status(200).send({ message: 'Everything is OK', data: await quizeCollection.findOne({ _id: ObjectId(req.params.qid) }) });
+            }
+            catch (err) {
+                res.status(500).send({ message: 'Internal server error', data: {} });
+            }
+        });
+        app.put('/');
+    }
+    finally {
+
+    }
+};
+main().catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('Hellso sWorld!');
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
