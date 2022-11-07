568d1c015f464f54d3dcf64a68421366b4894ce0
mahabubsaki <md929820@gmail.com>
2022-11-07 09:10:30 +0600

    done

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 9000;
 
-const uri = "mongodb+srv://mahabub3:JhYWH49UTIy2W6Oa@cluster0.wcxgg.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.wcxgg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-const quizes = require('./quizes.json');
 
 const main = async () => {
     const quizeCollection = client.db('triviamania').collection('quiztopics');
